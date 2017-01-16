c60b6ece2c3f84320c6b63c6287b76dff1b7bb16
Irene Canuti <irene.canuti@gmail.com>
2017-01-16 16:17:20 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  'HELLO!'
+end
+
+get '/secret' do
+  'goodbye!'
+end
+
+get '/cat' do
+
+"<div>
+<img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' style='border: 2px dotted red;'>
+</div>"
+
+end
